330923f08490980c84a9a796b745cb8333c2c1f8
Andrew Wong <andrew.j.wong@outlook.com>
2022-10-22 03:11:23 +1100

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,32 @@
 const fs = require('fs');
 const wasmBuffer = fs.readFileSync('a.out.wasm');
+
+const s_fixedkey = [23, 82, 107, 6, 35, 78, 88, 7]
+
 WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+    let enc = new TextEncoder()
+    let dec = new TextDecoder()
+
     const { deskey, des, memory } = wasmModule.instance.exports;
 
     let arr = new Uint8Array(memory.buffer)
-    arr.set([23, 82, 107, 6, 35, 78, 88, 7])
 
-    deskey(memory, 1);
-    console.log(wasmModule.instance.exports.test())
 
-    arr.set([0xd7, 0xa5, 0x14, 0xd8, 0xc5, 0x56, 0xaa, 0xde])
-    des(memory, memory);
+    const work = (data, mode) => {
+        arr.set(s_fixedkey)
+        deskey(memory, mode);
 
-    let dec = new TextDecoder()
+        arr.set(data)
+        des(memory, memory);
+    }
+    const encrypt = (password) => work(password, 0)
+    const decrypt = (bytes) => work(bytes, 1)
+
+
+    decrypt([0xd7, 0xa5, 0x14, 0xd8, 0xc5, 0x56, 0xaa, 0xde])
+
+    // encrypt(enc.encode("Securea!"))
     console.log(dec.decode(memory.buffer.slice(0, 8)));
     // let input = new Uint8Array([0xd7, 0xa5, 0x14, 0xd8, 0xc5, 0x56, 0xaa, 0xde])
     // let output = new Uint8Array(16)
-
-
-    // console.log(input, output);
-
-    // console.log('ok');
 });
