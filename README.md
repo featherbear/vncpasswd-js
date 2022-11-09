9977b6a87be75e00b2eb2d05c52e9288576590f8
Andrew Wong <andrew.j.wong@outlook.com>
2022-11-10 02:41:55 +1100

    Allow passing of an instance

---
+++ README.md
@@ -0,0 +1,7 @@
+# VNC Passwords
+
+Legacy VNC password utility in JS / WebAssembly
+
+---
+
+https://blog.featherbear.cc/post/vnc-passwords/
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
-export default async (wasmBuffer: BufferSource) => {
+export default async (wasmSource: BufferSource | WebAssembly.Instance) => {
+    let instance: WebAssembly.Instance
+    if (!(wasmSource instanceof WebAssembly.Instance)) {
+        instance = await WebAssembly.instantiate(wasmSource).then(s => s.instance)
+    } else {
+        instance = wasmSource
+    }
 
     const s_fixedkey = [23, 82, 107, 6, 35, 78, 88, 7]
 
-    const wasmModule = await WebAssembly.instantiate(wasmBuffer)
     let enc = new TextEncoder()
     let dec = new TextDecoder()
 
-    const { deskey, des }: { [fnName: string]: Function } = <any>wasmModule.instance.exports;
-    const { memory }: { memory: WebAssembly.Memory } = <any>wasmModule.instance.exports
+    const { deskey, des }: { [fnName: string]: Function } = <any>instance.exports;
+    const { memory }: { memory: WebAssembly.Memory } = <any>instance.exports
     let arr = new Uint8Array(memory.buffer)
 
     const work = (data: Uint8Array, mode: number) => {
         arr.set(s_fixedkey)
         deskey(memory, mode);
 
-        arr.fill(0,0,8)
+        arr.fill(0, 0, 8)
         arr.set(data)
         des(memory, memory);
     }
